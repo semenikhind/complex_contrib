bdccee0a35dae22acf7b1afadb29e0e24626a53f
Dmitry Semenikhin <semenikhindv@mail.ru>
2016-02-24 14:30:40 +0300

    successful add Complex type with WARNINGS

--- complex--1.0.sql
+++ complex--1.0.sql
@@ -3,6 +3,25 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION complex" to load this file. \quit
 
+CREATE FUNCTION complex_in(cstring)
+RETURNS complex
+AS 'MODULE_PATHNAME', 'complex_in'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION complex_out(complex)
+RETURNS cstring
+AS 'MODULE_PATHNAME', 'complex_out'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION complex_recv(internal)
+RETURNS complex
+AS 'MODULE_PATHNAME', 'complex_recv'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION complex_send(complex)
+RETURNS bytea
+AS 'MODULE_PATHNAME', 'complex_send'
+LANGUAGE C STRICT;
 
 CREATE TYPE complex (
 	internallength = 16,
